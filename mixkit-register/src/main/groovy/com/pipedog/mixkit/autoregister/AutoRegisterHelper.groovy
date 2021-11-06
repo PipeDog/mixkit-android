8f26c7092cf20e5a97dc8d0422bd27aa3d578a39
liang <leiliang4@xdf.cn>
2021-11-06 23:35:01 +0800

    format code style

--- mixkit-register/src/main/groovy/com/pipedog/mixkit/autoregister/AutoRegisterHelper.groovy
+++ mixkit-register/src/main/groovy/com/pipedog/mixkit/autoregister/AutoRegisterHelper.groovy
@@ -7,93 +7,84 @@ import java.lang.reflect.Type
 
 import static com.android.builder.model.AndroidProject.FD_INTERMEDIATES
 
-// 文件操作辅助类
+/* 文件操作辅助类 */
 class AutoRegisterHelper {
-    final static def CACHE_INFO_DIR = "auto-register"
 
-    /**
-     * 缓存自动注册配置的文件
-     * @param project
-     * @return file
-     */
+    private static final String CACHE_INFO_DIR = "auto-register"
+
+    /* 缓存自动注册配置的文件 */
     static File getRegisterInfoCacheFile(Project project) {
         String baseDir = getCacheFileDir(project)
-        if (mkdirs(baseDir)) {
-            return new File(baseDir + "register-info.config")
-        } else {
+        if (!mkdirs(baseDir)) {
             throw new FileNotFoundException("Not found  path:" + baseDir)
         }
+
+        return new File(baseDir + "register-info.config")
     }
 
-    /**
-     * 缓存扫描到结果的文件
-     * @param project
-     * @return File
-     */
+    /* 缓存扫描到结果的文件 */
     static File getRegisterCacheFile(Project project) {
         String baseDir = getCacheFileDir(project)
-        if (mkdirs(baseDir)) {
-            return new File(baseDir + "register-cache.json")
-        } else {
-            throw new FileNotFoundException("Not found  path:" + baseDir)
+        if (!mkdirs(baseDir)) {
+            throw new FileNotFoundException("Not found path: " + baseDir)
         }
+
+        return new File(baseDir + "register-cache.json")
     }
-    /**
-     * 将扫描到的结果缓存起来
-     * @param cacheFile
-     * @param harvests
-     */
+
+    /* 将扫描到的结果缓存起来 */
     static void cacheRegisterHarvest(File cacheFile, String harvests) {
-        if (!cacheFile || !harvests)
+        if (cacheFile == null || harvests == null) {
             return
+        }
+
         cacheFile.getParentFile().mkdirs()
-        if (!cacheFile.exists())
+        if (!cacheFile.exists()) {
             cacheFile.createNewFile()
+        }
+
         cacheFile.write(harvests)
     }
 
     private static String getCacheFileDir(Project project) {
-        return project.getBuildDir().absolutePath + File.separator + FD_INTERMEDIATES + File.separator + CACHE_INFO_DIR + File.separator
+        return project.getBuildDir().absolutePath +
+            File.separator +
+            FD_INTERMEDIATES +
+            File.separator +
+            CACHE_INFO_DIR +
+            File.separator
     }
 
     /**
-     * 读取文件内容并创建Map
+     * 读取文件内容并创建 Map
      * @param file 缓存文件
-     * @param type map的类型
-     * @return
+     * @param type map 的类型
      */
     static Map readToMap(File file, Type type) {
-        Map map = null
-        if (file.exists()) {
-            if (type) {
-                def text = file.text
-                if (text) {
-                    try {
-                        map = new Gson().fromJson(text, type)
-                    } catch (Exception e) {
-                        e.printStackTrace()
-                    }
-                }
-            }
+        Map map = new HashMap()
+
+        if (!file.exists()) { return map }
+        if (type == null) { return map }
+        if (file.text == null) { return map }
+
+        try {
+            map = new Gson().fromJson(file.text, type)
+        } catch (Exception e) {
+            e.printStackTrace()
+            return new HashMap()
         }
-        if (map == null) {
-            map = new HashMap()
-        }
+
         return map
     }
 
-    /**
-     * 创建文件夹
-     * @param dirPath
-     * @return boolean
-     */
+    /* 创建文件夹 */
     static boolean mkdirs(String dirPath) {
         def baseDirFile = new File(dirPath)
-        def isSuccess = true
-        if (!baseDirFile.isDirectory()) {
-            isSuccess = baseDirFile.mkdirs()
+        if (baseDirFile.isDirectory()) {
+            return true
         }
-        return isSuccess
+
+        return baseDirFile.mkdirs()
     }
 
 }
