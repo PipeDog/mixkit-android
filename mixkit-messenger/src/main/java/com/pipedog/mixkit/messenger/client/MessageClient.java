5b044b5fc7b1a7e637c69fc3842662905e85303c
liang <leiliang4@xdf.cn>
2021-11-22 15:19:27 +0800

    客户端代码提交（数据未封装）

--- mixkit-messenger/src/main/java/com/pipedog/mixkit/messenger/client/MessageClient.java
+++ mixkit-messenger/src/main/java/com/pipedog/mixkit/messenger/client/MessageClient.java
@@ -1,32 +1,128 @@
 package com.pipedog.mixkit.messenger.client;
 
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
-public class MessageClient {
+import com.pipedog.mixkit.messenger.constants.MessageNumber;
+import com.pipedog.mixkit.messenger.interfaces.IMessage2Server;
+import com.pipedog.mixkit.messenger.interfaces.IMessageCallback;
+import com.pipedog.mixkit.messenger.server.MessengerService;
+import com.pipedog.mixkit.tool.MixLogger;
 
+import java.util.Map;
+
+/**
+ * 客户端抽象类
+ * @author liang
+ * @time 2021/11/22
+ */
+public class MessageClient implements IMessage2Server {
+
+    private Context mContext;
     private Messenger mServerMessenger;
     private Messenger mClientMessenger = new Messenger(new ClientHandler());
+    private ServiceConnection mServiceConnection = new ProcessesConnection();
+    private boolean mIsConnected = false;
 
-    private ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            mServerMessenger = new Messenger(service);
+    private MessageClient() {
+        // Use `MessageClient(Context)` instead of current method
+    }
 
-            // TODO: 绑定 client 及注入 modules 信息到服务端等
+    public MessageClient(Context context) {
+        this.mContext = context;
+    }
+
+    /**
+     * 连接服务进程
+     * @return true 连接成功，false 连接失败
+     */
+    public boolean startConnection() {
+        boolean result = mContext.bindService(
+                new Intent(MessageClient.this, MessengerService.class),
+                mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        if (!result) {
+            MixLogger.error("Create connection with server process failed!");
+        }
+        return result;
+    }
+
+    /**
+     * 终止连接服务进程
+     */
+    public void stopConnection() {
+        mContext.unbindService(mServiceConnection);
+    }
+
+    /**
+     * 是否已连接
+     * @return true 已连接，false 未连接或连接失败
+     */
+    public boolean isConnected() {
+        return mIsConnected;
+    }
+
+    
+    // OVERRIDE METHODS FROM `IMessage2Server`
+
+    @Override
+    public void request2Server(String processId,
+                               String moduleName,
+                               String methodName,
+                               Map<String, Parcelable> parameter,
+                               IMessageCallback callback) {
+        if (mServerMessenger == null) {
+            return;
         }
 
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
+        Bundle bundle = new Bundle();
+        // TODO: 数据填充
 
+        try {
+            Message message = Message.obtain();
+            message.replyTo = mClientMessenger;
+            message.what = MessageNumber.REQUEST_TO_SERVER;
+            message.setData(bundle);
+            mServerMessenger.send(message);
+        } catch (Exception e) {
+            MixLogger.error("Request to server message send failed, error = %s", e.toString());
         }
     }
+
+    @Override
+    public void response2Server(String callbackId,
+                                Map<String, Parcelable> result,
+                                IMessageCallback callback) {
+        if (mServerMessenger == null) {
+            return;
+        }
+        
+        Bundle bundle = new Bundle();
+        // TODO: 数据填充
+
+        try {
+            Message message = Message.obtain();
+            message.replyTo = mClientMessenger;
+            message.what = MessageNumber.RESPONSE_TO_SERVER;
+            message.setData(bundle);
+            mServerMessenger.send(message);
+        } catch (Exception e) {
+            MixLogger.error("Request to server message send failed, error = %s", e.toString());
+        }
+    }
+
+
+    // INTERNAL CLASSES
 
     private class ClientHandler extends Handler {
         @Override
@@ -34,6 +130,22 @@ public class MessageClient {
             super.handleMessage(msg);
 
             // TODO: 指令处理
+        }
+    }
+
+    private class ProcessesConnection extends ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mServerMessenger = new Messenger(service);
+            mIsConnected = true;
+
+            // TODO: 绑定 client 及注入 modules 信息到服务端等
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServerMessenger = null;
+            mIsConnected = false;
         }
     }
 
---
+++ mixkit-messenger/src/main/java/com/pipedog/mixkit/messenger/core/MessengerManager.java
@@ -0,0 +1,104 @@
+package com.pipedog.mixkit.messenger.core;
+
+import android.content.Context;
+import android.os.Message;
+import android.os.Parcelable;
+
+import com.pipedog.mixkit.launch.MixLaunchManager;
+import com.pipedog.mixkit.messenger.client.MessageClient;
+import com.pipedog.mixkit.messenger.interfaces.IMessage2Server;
+import com.pipedog.mixkit.messenger.interfaces.IMessageCallback;
+import com.pipedog.mixkit.tool.MixLogger;
+
+import java.util.Map;
+
+/**
+ * 夸进程通信入口类
+ * @author liang
+ * @time 2021/11/22
+ */
+public class MessengerManager implements IMessengerBridgeDelegate, IMessage2Server {
+
+    private MessengerBridge mBridge;
+    private MessageClient mClient;
+    private volatile static MessengerManager sMessengerManager;
+
+    public static MessengerManager getInstance() {
+        if (sMessengerManager == null) {
+            synchronized (MessengerManager.class) {
+                if (sMessengerManager == null) {
+                    sMessengerManager = new MessengerManager();
+                }
+            }
+        }
+        return sMessengerManager;
+    }
+
+    private MessengerManager() {
+        mBridge = new MessengerBridge(this);
+        
+        Context context = MixLaunchManager.defaultManager().getContext();
+        if (context == null) {
+            throw new Exception(
+                    "Call method `registerContext` in class `MixLaunchManager` first!");
+        }
+        mClient = new MessageClient(context);
+    }
+
+    /**
+     * 启动连接
+     * @return true 连接成功，false 连接失败
+     */
+    public boolean startConnection() {
+        return mClient.startConnection();
+    }
+
+    /**
+     * 终止连接服务进程
+     */
+    public void stopConnection() {
+        mClient.stopConnection();
+    }
+
+
+    // OVERRIDE METHOD FROM `IMessengerBridgeDelegate`
+
+    @Override
+    public IMessage2Server serverCaller() {
+        return mClient;
+    }
+
+
+    // OVERRIDE METHODS FROM `IMessage2Server`
+
+    @Override
+    public void request2Server(String processId,
+                               String moduleName,
+                               String methodName,
+                               Map<String, Parcelable> parameter,
+                               IMessageCallback callback) {
+        if (!mClient.isConnected()) {
+            if (callback != null) {
+                callback.callback("Client dose not connect server!", null);
+            }
+            return;
+        }
+
+        mClient.request2Server(processId, moduleName, methodName, parameter, callback);
+    }
+
+    @Override
+    public void response2Server(String callbackId,
+                                Map<String, Parcelable> result,
+                                IMessageCallback callback) {
+        if (!mClient.isConnected()) {
+            if (callback != null) {
+                callback.callback("Client dose not connect server!", null);
+            }
+            return;
+        }
+
+        mClient.response2Server(callbackId, result, callback);
+    }
+
+}
