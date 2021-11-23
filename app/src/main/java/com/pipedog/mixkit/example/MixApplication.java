37e6e85975de4dd96127321745e51567d7ed0425
liang <leiliang4@xdf.cn>
2021-11-23 18:08:05 +0800

    demo 添加

--- app/src/main/java/com/pipedog/mixkit/example/MixApplication.java
+++ app/src/main/java/com/pipedog/mixkit/example/MixApplication.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.app.Application;
 
 import com.pipedog.mixkit.kernel.MixResultCallback;
+import com.pipedog.mixkit.messenger.IMessengerEngine;
 import com.pipedog.mixkit.messenger.MessengerEngine;
 
 import java.util.Arrays;
@@ -19,19 +20,36 @@ public class MixApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        MessengerEngine engine = MessengerEngine.getInstance();
+
+        // 1、初始化配置
+        engine.setupConfiguration(new IMessengerEngine.IConfigurationCallback() {
+            @Override
+            public void setup(IMessengerEngine.IInitialConfiguration configuration) {
+                configuration.setContext(getApplicationContext());
+                configuration.setClientId("com.client.mainapp");
+
+                // action 及 package 需要在 AndroidManifest.xml 中配置
+                configuration.setAction("");
+                configuration.setPackage("");
+            }
+        });
+
+        // 2、启动引擎
+        engine.launch();
+
+        // 3、调用其他进程的方法并获取回调
         String clientId = "";
         String moduleName = "";
         String methodName = "";
 
-        // Arrays.asList("foo","bar");
-        MessengerEngine.getInstance().sendMessage(clientId, moduleName,
-                methodName, Arrays.asList("argument 1", "argument 2", new MixResultCallback() {
+        engine.sendMessage(clientId, moduleName, methodName,
+                Arrays.asList("argument 1", "argument 2", new MixResultCallback() {
             @Override
             public void invoke(Object[] arguments) {
 
             }
         }));
-
     }
 
 }
