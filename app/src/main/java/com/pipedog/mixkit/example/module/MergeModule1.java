f9045f29bf6169319354573a871d4574e695faf1
liang <leiliang4@xdf.cn>
2022-04-24 17:32:15 +0800

    demo update

--- app/src/main/java/com/pipedog/mixkit/example/module/MergeModule1.java
+++ app/src/main/java/com/pipedog/mixkit/example/module/MergeModule1.java
@@ -2,10 +2,15 @@ package com.pipedog.mixkit.example.module;
 
 import com.pipedog.mixkit.annotation.MixMethod;
 import com.pipedog.mixkit.annotation.MixModule;
+import com.pipedog.mixkit.kernel.IBridge;
+import com.pipedog.mixkit.kernel.IBridgeModule;
 import com.pipedog.mixkit.tool.MixLogger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @MixModule(name = "TestMergeModule")
-public class MergeModule1 {
+public class MergeModule1 implements IBridgeModule {
 
     @MixMethod(name = "log1")
     public void log() {
@@ -21,5 +26,18 @@ public class MergeModule1 {
     public void test2() {
 
     }
+
+    @Override
+    public void setBridge(IBridge bridge) {
+
+    }
+
+    public static Map<String, Object> constantsToExport() {
+        MixLogger.error(">>>>> class : MergeModule1 constantsToExport");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("version", "v1.0.0");
+        return map;
+    };
 
 }
--- app/src/main/java/com/pipedog/mixkit/example/module/MergeModule2.java
+++ app/src/main/java/com/pipedog/mixkit/example/module/MergeModule2.java
@@ -2,10 +2,15 @@ package com.pipedog.mixkit.example.module;
 
 import com.pipedog.mixkit.annotation.MixMethod;
 import com.pipedog.mixkit.annotation.MixModule;
+import com.pipedog.mixkit.kernel.IBridge;
+import com.pipedog.mixkit.kernel.IBridgeModule;
 import com.pipedog.mixkit.tool.MixLogger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @MixModule(name = "TestMergeModule")
-public class MergeModule2 {
+public class MergeModule2 implements IBridgeModule {
 
     @MixMethod(name = "print")
     public void print() {
@@ -25,6 +30,21 @@ public class MergeModule2 {
     @MixMethod(name = "test101")
     public void test101() {
 
+    }
+
+    @Override
+    public void setBridge(IBridge bridge) {
+
+    }
+
+    public static Map<String, Object> constantsToExport() {
+        MixLogger.error(">>>>> class : MergeModule2 constantsToExport");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("module2Value", "this is module2");
+        map.put("intValue", 90);
+        map.put("floatValue", 100.001);
+        return map;
     }
 
 }
